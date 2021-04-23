4b6a9253244a2b832ab98d75113bcb049e02f523
AsylzhanUbaidullaev <asylzhan18092001@gmail.com>
2021-04-23 13:48:17 +0600

    lab 8

---
+++ src/main/resources/db.migration/V1__create_users.sql
@@ -0,0 +1,6 @@
+CREATE table users (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+)
---
+++ src/main/resources/db.migration/V2__fill_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(name, email, password) VALUES
+('Asylzhan', 'asylzhan@gmail.com', '123'),
+('Arlan', 'arlan@gmail.com', '123'),
+('Aliomar', 'aliomar@gmail.com', '123')
